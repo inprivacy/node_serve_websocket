9cf6e3a7edacb3461415f52ca15cd07783a518c8
lvyan <ly@LideMac-mini.local>
2020-08-20 10:54:43 +0800

    readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # node_serve_websocket
 node搭建的websocket服务器
+## 创建步骤
+1. 创建 node_serve_websocket文件夹
+2. 创建 app.js
+3. 如果没有 package.json，则创建 
+   `npm init`
+4. 安装 nodejs-websocket 
+   `npm install nodejs-websocket`
+5. 运行
+   `node app.js`   
+
--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// var http = require("http");
-// http.createServer(function (req, res) {
-//     res.write('<head><meta charset="utf-8"/></head>');
-//     res.write('hello word');
-//     res.end();
-// }).listen(3000);
-
-
 //websocket
 const ws = require('nodejs-websocket');
 
