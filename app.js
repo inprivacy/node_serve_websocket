57d1a91ca9d16ae2fe5e403c85409c1d6579aafd
lvyan <ly@LideMac-mini.local>
2020-08-20 18:13:50 +0800

    发送数据

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
 //websocket
 const ws = require('nodejs-websocket');
+require('./data/seda');
 
-var server = ws.createServer(function (connection) {
-    console.log('hello word');
+const server = ws.createServer(function (connection) {
+    console.log('有客户端连接');
+    var id = 1;
+    // var sendEndpoint;
+    // sendProject;
+
+    var sendEndpoint = setInterval(() => {
+        if (id <= 7) {
+            // console.log(id);
+            connection.send(JSON.stringify(endpoint_status(id)));
+        } else {
+            id = 0;
+        }
+        id++;
+
+    }, 10000);
+
+    var sendProject = setInterval(() => {
+        connection.send(JSON.stringify(project_status()));
+    }, 15000);
+
     //接收到字符串str
     connection.on("text", function (str) {
         console.log(str);
+        if (str === 'hear_beat') {
+            connection.send(JSON.stringify({ msg_type: 'hear_beat', content: 'hear_beat' }));
+        }
     });
 
     connection.on("close", function () {
         //客户端关闭
         console.log('客户端关闭');
-
+        clearInterval(sendEndpoint);
+        clearInterval(sendProject);
     });
 
     connection.on("error", function () {
         //error
         console.log('error');
-
+        clearInterval(sendEndpoint);
+        clearInterval(sendProject);
     });
 
 });
---
+++ data/seda.js
@@ -0,0 +1,41 @@
+endpoint_status = (id) => {
+    return {
+        msg_type: "endpoint_status",
+        content: {
+            endpoint_id: 'endpoint_id_0' + id,
+            records: {
+                record_01: (Math.random() * 20).toFixed(2),
+                record_02: (Math.random() * 20).toFixed(2),
+                record_03: (Math.random() * 20).toFixed(2),
+                record_04: Math.random().toFixed(2) > 0.5 ? 1 : 0,
+                record_05: Math.random().toFixed(2) > 0.5 ? 1 : 0,
+                record_06: Math.random().toFixed(2) > 0.5 ? 1 : 0,
+                record_07: Math.random().toFixed(2) > 0.5 ? 1 : 0,
+                record_08: Math.random().toFixed(2) > 0.5 ? 1 : 0,
+                record_09: Math.random().toFixed(2) > 0.5 ? 1 : 0,
+            }
+        }
+    };
+}
+
+project_status = () => {
+    return {
+        msg_type: "project_status",
+        content: {
+            endpoint_usage: {
+                endpoint_id_01: (Math.random() * 20).toFixed(2),
+                endpoint_id_02: (Math.random() * 20).toFixed(2),
+                endpoint_id_03: (Math.random() * 20).toFixed(2),
+                endpoint_id_04: (Math.random() * 20).toFixed(2),
+                endpoint_id_05: (Math.random() * 20).toFixed(2),
+                endpoint_id_06: (Math.random() * 20).toFixed(2),
+                endpoint_id_07: (Math.random() * 20).toFixed(2),
+            },
+            weather: {
+                temprature: ((Math.random() + 2) * 10).toFixed(2),
+                humidity: (Math.random() * 100).toFixed(2),
+                presure: (Math.random() * 100).toFixed(2),
+            }
+        }
+    };
+}
